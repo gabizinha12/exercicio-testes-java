57d454fcb71483f578af318238a6b8caa6f4599f
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-02-13 21:54:07 -0300

    add resolução

--- src/tests/entities/FinancingTests.java
+++ src/tests/entities/FinancingTests.java
@@ -11,7 +11,9 @@ public class FinancingTests {
 	@Test
 	public void shouldCreateObjectWhenValidConstructor() {
 		Financing financing = new Financing(100000.0, 2000.0, 80);
-		Assertions.assertDoesNotThrow(() -> IllegalArgumentException.class);
+		Assertions.assertEquals(100000.0, financing.getTotalAmount());
+		Assertions.assertEquals(2000.0, financing.getIncome());
+		Assertions.assertEquals(80, financing.getMonths());
 
 	}
 
@@ -23,18 +25,18 @@ public class FinancingTests {
 	}
 
 	@Test
-	public void totalAmountShouldUpdateWhenValid() {
+	public void setTotalAmountShouldUpdateWhenValid() {
 		Double expectedValue = 100000.0;
-		Financing financing = new Financing(100000.0, 2000.0, 80);
-		//financing.setTotalAmount(100000.0);
+		Financing financing = new Financing(0.00, 2000.0, 80);
+		financing.setTotalAmount(100000.0);
 		Assertions.assertEquals(financing.getTotalAmount(), expectedValue);
 	}
 
 	@Test
-	public void totalAmountShouldThrowExceptionWhenInvalid() {
+	public void setTotalAmountShouldThrowIllegalArgumentExceptionWhenInvalid() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			Financing financing = new Financing(00.00, 2000.0, 20);
-			financing.setTotalAmount(100000.0);
+			Financing financing = new Financing(100000.0, 2000.0, 20);
+			financing.setTotalAmount(110000.0);
 		});
 
 	}
@@ -42,8 +44,8 @@ public class FinancingTests {
 	@Test
 	public void incomeShouldUpdateWhenValid() {
 		Financing financing = new Financing(100000.0, 2000.0, 80);
-	//	financing.setIncome(2000.0);
-		Assertions.assertEquals(2000.0, 2000.0, 0);
+		financing.setIncome(2200.0);
+		Assertions.assertEquals(financing.getIncome(), 2200.0);
 
 	}
 
@@ -51,33 +53,33 @@ public class FinancingTests {
 	public void incomeShouldThrowExceptionWhenInvalid() {
 		Assert.assertThrows(IllegalArgumentException.class, () -> {
 			Financing financing = new Financing(100000.0, 00.00, 20);
-		    financing.setIncome(1000.0);
+			financing.setIncome(1000.0);
 		});
 
 	}
-	
+
 	@Test
 	public void shouldUpdateMonthsWhenValid() {
 		Financing financing = new Financing(1000.0, 2000.0, 20);
 		financing.setMonths(80);
-		Assertions.assertEquals(80, 80, 0);
+		Assertions.assertEquals(financing.getMonths(), 80);
 	}
-	
+
 	@Test
 	public void shouldThrowExceptionWhenMonthsAreInvalid() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			Financing financing = new Financing(1000.0, 2000.0, 0);
-			financing.setMonths(20);
+			financing.setMonths(5);
 		});
-		
+
 	}
-	
+
 	@Test
 	public void entryShouldBeValid() {
 		Financing financing = new Financing(1000.0, 2000.0, 80);
 		Assertions.assertEquals(200.0, financing.entry());
 	}
-	
+
 	@Test
 	public void quotaShouldBeValid() {
 		Financing financing = new Financing(1000.0, 2000.0, 80);
